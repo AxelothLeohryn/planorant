fb7672310d3860fa9fadf6dacf9997057154d95d
AxelothLeohryn <alejandro97ss@gmail.com>
2024-02-28 22:03:18 +0100

    add Vite config for deployment

--- index.js
+++ index.js
@@ -19,6 +19,16 @@ const userRoutes = require("./routes/user.routes.js");
 app.use("/api", apiRoutes);
 // app.use("/user", userRoutes);
 
+//* Serve static assets in production, must be at this location of this file
+if (process.env.NODE_ENV === "production") {
+  //*Set static folder (VITE --> dist)
+  app.use(express.static("client/dist"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"))
+  );
+}
+
 app.listen(PORT, () => {
   console.log(`Planorant Backend is running on port ${PORT}`);
 });

ac3ceb375feb277b06299203470f50a87df51a54
Alejandro Sanchez <133040928+AxelothLeohryn@users.noreply.github.com>
2024-02-29 23:46:52 +0100

    Update ChatComponent.jsx

--- client/src/components/Main/PlannerSection/Planner/ChatComponent/ChatComponent.jsx
+++ client/src/components/Main/PlannerSection/Planner/ChatComponent/ChatComponent.jsx
@@ -15,7 +15,7 @@ const ChatComponent = ({ playersData }) => {
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    const newSocket = socketIO.connect("https://planorant.onrender.com:5000");
+    const newSocket = socketIO.connect();
     setSocket(newSocket);
   
     // Emit the newUser event immediately after connecting
